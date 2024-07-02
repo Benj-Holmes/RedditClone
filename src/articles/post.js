dbdfbfe874ce1434c016cdc3d166f3c5117066f5
Benj-Holmes <HolmesBen@Proton.me>
2024-07-02 12:56:38 +0100

    added further styling to articles

--- src/articles/post.js
+++ src/articles/post.js
@@ -4,30 +4,35 @@ import PostIcon from '../imgs/ThumbnailReplacer.jpeg';
 
 const Post = (props) => {
     return (
-        <article className='post'>
-            <div className='postTop'>
-                <p>{props.post.subreddit_name_prefixed}</p>
-                <p>u/{props.post.author}</p>
-            </div>
-            <div className='postTitle'>
-                <h3>{props.post.title}</h3>
-            </div>
-            <div className='postMain'>
-                {/* This Ternary checks for a video, which some posts have instead of a text body, if so it posts it */}
-                {(props.post.media?.reddit_video != null) ? <video controls src={props.post.media.reddit_video.fallback_url}/>: ''}
+        <div>
+            <article className='post'>
+                <div className='postTop'>
+                    <p>{props.post.subreddit_name_prefixed}</p>
+                    <p>u/{props.post.author}</p>
+                </div>
+                <div className='postTitle'>
+                    <h3>{props.post.title}</h3>
+                </div>
+                <div className='postMain'>
+                    {/* This Ternary checks for a video, which some posts have instead of a text body, if so it posts it */}
+                    {(props.post.media?.reddit_video != null) ? <video controls src={props.post.media.reddit_video.fallback_url}/>: ''}
 
-                {(props.post.selftext) ? <p>{props.post.selftext}</p> : ''}
+                    {(props.post.selftext) ? <p>{props.post.selftext}</p> : ''}
 
-                {/* This Ternary checks for a thumbnail and posts a generic one if it wasn't returned from the API */}
-                {(props.post.thumbnail !== 'self' && props.post.thumbnail !== 'default') ? 
-                <img className='thumbnail' src={props.post.thumbnail} alt='' 
-                style={{width: props.post.thumnbnail_width, height: props.post.thumnbnail_height}} /> :
-                <img className='thumbnailReplacement' src={PostIcon} alt='' /> }
-            </div>
-            <div className='postBottom'>
+                    {/* This Ternary checks for a thumbnail and posts a generic one if it wasn't returned from the API */}
+                    {(props.post.thumbnail !== 'self' && props.post.thumbnail !== 'default') ? 
+                    <img className='thumbnail' src={props.post.thumbnail} alt='' 
+                    style={{width: props.post.thumnbnail_width, height: props.post.thumnbnail_height}} /> :
+                    <img className='thumbnailReplacement' src={PostIcon} alt='' /> }
+                </div>
+                <div className='postBottom'>
+
+                </div>
+            </article>
+            <div className='postbreak'>
 
             </div>
-        </article>
+        </div>
     );
 }
 
